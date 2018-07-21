b15891c008da93b5a79b627360fb03425adfeb84
Suhas Bharadwaj <suhas.five@gmail.com>
2018-07-22 00:05:56 +0530

    Added Bifrost packages and implemented switching and toggling of the device attached to the hardcoded GPIO pin via HTTP calls.

--- Controllers/DevicesController.cs
+++ Controllers/DevicesController.cs
@@ -1,3 +1,6 @@
+using Bifrost.Devices.Gpio;
+using Bifrost.Devices.Gpio.Abstractions;
+using Bifrost.Devices.Gpio.Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeControl.Controllers
@@ -6,17 +9,62 @@ namespace HomeControl.Controllers
     public class DevicesController : Controller
     {
         static bool IsDeviceOn;
+        int pinId = 2;
 
-        [HttpGet("state")]
-        public string GetDeviceState()
+        private IGpioController GpioController { get; set; }
+
+        private IGpioPin GpioPin { get; set; }
+
+
+        public DevicesController()
         {
-            return IsDeviceOn ? "On" : "Off";
+            this.GpioController = Bifrost.Devices.Gpio.GpioController.Instance;
+            var pin = this.GpioController.OpenPin(this.pinId);
         }
 
-        [HttpPost("toggle")]
-        public string ToggleDevice()
+        [HttpGet("state")]
+        public dynamic GetDeviceState()
         {
+            return new { IsDeviceOn };
+        }
+
+        [HttpGet("toggle")]
+        public dynamic ToggleDevice()
+        {
+            this.GpioPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            if (IsDeviceOn)
+            {
+                this.GpioPin.Write(GpioPinValue.Low);
+            }
+            else
+            {
+                this.GpioPin.Write(GpioPinValue.High);
+            }
+
             IsDeviceOn = !IsDeviceOn;
+            return this.GetDeviceState();
+        }
+
+        [HttpPost("on")]
+        public dynamic SwitchOnDevice()
+        {
+            if (!IsDeviceOn)
+            {
+                this.GpioPin.Write(GpioPinValue.High);
+            }
+
+            return this.GetDeviceState();
+        }
+
+        [HttpPost("post")]
+        public dynamic SwitchOffDevice()
+        {
+            if (IsDeviceOn)
+            {
+                this.GpioPin.Write(GpioPinValue.Low);
+            }
+
             return this.GetDeviceState();
         }
     }
